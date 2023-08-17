c8d6b8be5c5ba2f2243ae62498bc274b22402fb8
Petyo Ivanov <underlog@gmail.com>
2023-08-17 16:50:32 +0300

    Initial setup

---
+++ pages/Editor.tsx
@@ -0,0 +1,35 @@
+// You can use this code in a separate component that's imported in your pages.
+import type { CodeBlockEditorDescriptor } from '@mdxeditor/editor';
+import '@mdxeditor/editor/style.css';
+import React from 'react';
+const { MDXEditor , codeBlockPlugin, headingsPlugin, listsPlugin, linkPlugin, quotePlugin, markdownShortcutPlugin, useCodeBlockEditorContext } = await import('@mdxeditor/editor')
+
+const PlainTextCodeEditorDescriptor: CodeBlockEditorDescriptor = {
+  match: () => true,
+  priority: 0,
+  Editor: (props) => {
+    const cb = useCodeBlockEditorContext()
+    return (
+      <div onKeyDown={(e) => e.nativeEvent.stopImmediatePropagation()}>
+        <textarea rows={3} cols={20} defaultValue={props.code} onChange={(e) => cb.setCode(e.target.value)} />
+      </div>
+    )
+  }
+}
+
+const Editor = () => {
+    return <MDXEditor
+      onChange={console.log}
+      markdown={'Hello world!'}
+      plugins={[
+        codeBlockPlugin({ codeBlockEditorDescriptors: [PlainTextCodeEditorDescriptor] }),
+        headingsPlugin(),
+        listsPlugin(),
+        linkPlugin(),
+        quotePlugin(),
+        markdownShortcutPlugin()
+      ]}
+    />
+}
+
+export default Editor
