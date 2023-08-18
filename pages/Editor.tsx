22d7fc94dc5f3b0e38f0c4c0763a98c0ad1ed0d0
Petyo Ivanov <underlog@gmail.com>
2023-08-18 15:11:09 +0300

    Comment box like configuration

--- pages/Editor.tsx
+++ pages/Editor.tsx
@@ -1,33 +1,62 @@
 // You can use this code in a separate component that's imported in your pages.
-import type { CodeBlockEditorDescriptor } from '@mdxeditor/editor';
+import { toolbarPlugin, corePluginHooks, imagePlugin, imagePluginHooks } from '@mdxeditor/editor';
+import {$getSelection, $isRangeSelection} from 'lexical'
 import '@mdxeditor/editor/style.css';
-import React from 'react';
-const { MDXEditor , codeBlockPlugin, headingsPlugin, listsPlugin, linkPlugin, quotePlugin, markdownShortcutPlugin, useCodeBlockEditorContext } = await import('@mdxeditor/editor')
+import React, { useCallback } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+const { MDXEditor , headingsPlugin, listsPlugin, linkPlugin, quotePlugin, markdownShortcutPlugin } = await import('@mdxeditor/editor')
 
-const PlainTextCodeEditorDescriptor: CodeBlockEditorDescriptor = {
-  match: () => true,
-  priority: 0,
-  Editor: (props) => {
-    const cb = useCodeBlockEditorContext()
-    return (
-      <div onKeyDown={(e) => e.nativeEvent.stopImmediatePropagation()}>
-        <textarea rows={3} cols={20} defaultValue={props.code} onChange={(e) => cb.setCode(e.target.value)} />
-      </div>
-    )
-  }
+// using the emoji picker is just an example, you can put it in a dropdown, popover, etc. 
+const EmojiButton = () => {
+  const [editor] = corePluginHooks.useEmitterValues('activeEditor')
+  const insertText = useCallback((text: string) => {
+      editor?.focus(() => {
+        editor?.update(() => {
+          const selection = $getSelection() 
+          if (selection && $isRangeSelection(selection)) {
+            // the code point below is a smiley face emoji https://www.compart.com/en/unicode/U+1F600
+            // You can find more code points here: https://unicode-table.com/en/
+            selection.insertText(text)
+            // the picker used steals the focus, restore it after the emoji is inserted
+            setTimeout(() => {
+              editor.focus()
+            }, 100)
+          }
+        })
+      })
+  }, [editor])
+
+  return <EmojiPicker onEmojiClick={(emoji) => insertText(emoji.emoji)} />
+}
+
+// a simple button that inserts a gif. 
+// You can use a fancier picker like the tenor picker: https://github.com/MrBartusek/gif-picker-react
+const GifButton = () => {
+  const [editor] = corePluginHooks.useEmitterValues('activeEditor')
+  const insertImage = imagePluginHooks.usePublisher('insertImage')
+  const onClick = useCallback((url: string) => {
+    editor?.focus(() => {
+      insertImage(url)
+    })
+  }, [insertImage, editor])
+
+  return <button style={{background: 'red', color: 'white', padding: '2rem' }} onClick={() => onClick('https://media.giphy.com/media/3o7aD2jB0EFzYxY9fK/giphy.gif')}>Gif</button>
 }
 
 const Editor = () => {
     return <MDXEditor
+      autoFocus
       onChange={console.log}
       markdown={'Hello world!'}
       plugins={[
-        codeBlockPlugin({ codeBlockEditorDescriptors: [PlainTextCodeEditorDescriptor] }),
         headingsPlugin(),
         listsPlugin(),
         linkPlugin(),
         quotePlugin(),
-        markdownShortcutPlugin()
+        markdownShortcutPlugin(),
+        toolbarPlugin({toolbarContents: () => {return <><GifButton /> <EmojiButton /></>}}),
+        // disable image resize, you probably don't want this
+        imagePlugin({disableImageResize: true})
       ]}
     />
 }
