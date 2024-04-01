1a9cf58ed676fc2103d609faef1dca15d1077038
Petyo Ivanov <underlog@gmail.com>
2024-04-01 17:25:34 +0300

    Notice on the example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
-import { GetServerSideProps } from 'next'
+import React from "react";
+import dynamic from "next/dynamic";
 
-const Editor = dynamic(() => import('../components/Editor'), { ssr: false })
+const Editor = dynamic(() => import("../components/Editor"), { ssr: false });
 
 export default function Home() {
   return (
     <>
+      <p>
+        This is a bare-bones unstyled MDX editor without any plugins and no
+        toolbar. Check the components/Editor.tsx file for the code.
+      </p>
+      <p>
+        To enable more features, add the respective plugins to your instance -
+        see{" "}
+        <a href="https://mdxeditor.dev/editor/docs/getting-started">the docs</a>{" "}
+        for more details.
+      </p>
+      <br />
       <Editor />
     </>
-  )
+  );
 }
